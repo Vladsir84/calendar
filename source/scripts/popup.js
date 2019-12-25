2fafe0e371a8721d748be7ba57afc122bfba5247
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-25 17:20:12 +0200

    Update popup.js

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,45 +1,45 @@
-const addButton = (event) => {
-    const popup = document.querySelector(`.popup`);
-    popup.classList.add('popup-switch');
-    if (event.target.classList.value === '.emptyCell') {
-        btnUpdate.classList.remove("update")
-      return
-    }
-}
+// const addButton = (event) => {
+//     const popup = document.querySelector(`.popup`);
+//     popup.classList.add('popup-switch');
+//     if (event.target.classList.value === '.emptyCell') {
+//         btnUpdate.classList.remove("update")
+//       return
+//     }
+// }
 
-const timeListElemStart = document.querySelector('.start-time');
-const timeListElemEnd = document.querySelector('.end-time');
-const popupWindow = document.querySelector(`.calendar-visualization`);
+// const timeListElemStart = document.querySelector('.start-time');
+// const timeListElemEnd = document.querySelector('.end-time');
+// const popupWindow = document.querySelector(`.calendar-visualization`);
 
-const btnCreate = document.querySelector('.create-button');
+// const btnCreate = document.querySelector('.create-button');
 
-btnCreate.addEventListener('click', addButton);
+// btnCreate.addEventListener('click', addButton);
 
-const TimeElem = () => {
-    let resultTime = [];
+// const TimeElem = () => {
+//     let resultTime = [];
 
-    generateNumbersRange(0, 23)
-        .map(timeList => {
-            let setTime = '';
-            let setMin = 0;
-            let cell = '00';
-            for (let i = 0; i < 6; i++) {
-                timeList < 10 ? setTime = `0${timeList}` : setTime = timeList;
-                resultTime.push(
-                    `<option 
-            value="${setTime}:${cell}"
-            data-block-number='${timeList}'>${setTime}:${cell}</option> `)
-                setMin += 10;
-                cell = setMin;
-            };
-        })
+//     generateNumbersRange(0, 23)
+//         .map(timeList => {
+//             let setTime = '';
+//             let setMin = 0;
+//             let cell = '00';
+//             for (let i = 0; i < 6; i++) {
+//                 timeList < 10 ? setTime = `0${timeList}` : setTime = timeList;
+//                 resultTime.push(
+//                     `<option 
+//             value="${setTime}:${cell}"
+//             data-block-number='${timeList}'>${setTime}:${cell}</option> `)
+//                 setMin += 10;
+//                 cell = setMin;
+//             };
+//         })
 
-    return resultTime.join('');
-}
+//     return resultTime.join('');
+// }
 
-const renderTimeList = () => {
-    timeListElemStart.innerHTML = TimeElem();
-    timeListElemEnd.innerHTML = TimeElem();
-}
+// const renderTimeList = () => {
+//     timeListElemStart.innerHTML = TimeElem();
+//     timeListElemEnd.innerHTML = TimeElem();
+// }
 
-renderTimeList()
+// renderTimeList()
