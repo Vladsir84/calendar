78c2dd9710c86745c8777b8bc4c9f2e13affda31
“Il_Roman” <il.roman@ukr.net>
2019-12-25 19:56:52 +0200

    commit

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,5 @@
 import { calendarRendering } from './calendar-visualization.js';
-import { createDates, renderDates, showCurrentMonthAndYear, renderNextWeek, renderPreviousWeek, today_button_function } from './navigation.js';
+import { arrayOfDates, createDates, renderDates, showCurrentMonthAndYear, renderNextWeek, renderPreviousWeek, today_button_function } from './navigation.js';
 import { generateNumbersRange, getTimesBlocks, } from './main.js';
-import { emptyCellClick } from './slots-logic.js';
+import { emptyCellClick, displayEvent } from './slots-logic.js';
 import { addButton } from './popup.js';
