5f21a0b1aefa491beadd63aa4c3d08e8512fe7de
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-25 17:06:38 +0200

    Update popup.js

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -3,19 +3,15 @@ const addButton = (event) => {
     popup.classList.add('popup-switch');
     if (event.target.classList.value === '.emptyCell') {
         btnUpdate.classList.remove("update")
-
-        return
+      return
     }
-
 }
 
 const timeListElemStart = document.querySelector('.start-time');
 const timeListElemEnd = document.querySelector('.end-time');
 const popupWindow = document.querySelector(`.calendar-visualization`);
 
-
 const btnCreate = document.querySelector('.create-button');
-
 
 btnCreate.addEventListener('click', addButton);
 
