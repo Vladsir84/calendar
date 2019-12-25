d15f38f6c8306251a1172bba6e1877c7660930f5
“Il_Roman” <il.roman@ukr.net>
2019-12-25 15:28:34 +0200

    Отрисовка ивентов

--- source/scripts/slots-logic.js
+++ source/scripts/slots-logic.js
@@ -12,18 +12,22 @@ export const emptyCellClick = event => {
 calendar_visualization.addEventListener('click', emptyCellClick);
 
 function displayEvent(starttime, endTime, dayNumber, text) {
+    let diff = ((endTime - starttime) / 1000 / 60);
     let eventPlace = document.querySelector('.event-place')
     let activeEvent = document.createElement('div');
     activeEvent.classList.add('active_event');
-    activeEvent.style.height = `${(endTime - starttime) * 50}px`;
-    activeEvent.style.marginLeft = `${60 + dayNumber * 209}px`
-    activeEvent.style.marginTop = `${starttime * 50}px`;
-
+    activeEvent.style.height = `${diff}px`;
+    activeEvent.style.marginLeft = `${60 + dayNumber * 208}px`
+    activeEvent.style.marginTop = `${diff}px`;
     eventPlace.append(activeEvent);
     activeEvent.innerHTML += `${text}<br>`
-    activeEvent.innerHTML += `${(starttime < 10) ? '0' + starttime : starttime + ''}: 00 - ${(starttime < 10) ? '0' + (starttime + 1) : starttime + 1 + ''}: 00`;
+    activeEvent.innerHTML += `${(starttime + '').split(' ')[4]} - ${(endTime + '').split(' ')[4]}`;
 };
 
 let x = new Date('December 17, 1995 03:24:00');
-let y = new Date('December 17, 1995 03:54:00');
+let y = new Date('December 17, 1995 04:54:00');
 displayEvent(x, y, 5, 'Новое задание');
+
+let x1 = new Date('December 17, 1995 00:05:00');
+let y1 = new Date('December 17, 1995 01:54:00');
+displayEvent(x1, y1, 3, 'Новое задание');
