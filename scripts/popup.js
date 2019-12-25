7235dfb8b094024f5f55b711aea5904b7715bbff
John Doe <vperedz@gmail.com>
2019-12-25 17:24:44 +0200

    add

---
+++ scripts/popup.js
@@ -0,0 +1,15 @@
+const addButton = (event) => {
+    const popup = document.querySelector(`.popup`);
+    popup.classList.add('popup-switch');
+    if (event.target.classList.value === '.emptyCell') {
+        btnUpdate.classList.remove("update")
+
+        return
+    }
+
+}
+
+const popupWindow = document.querySelector(`.calendar-visualization`);
+const btnCreate = document.querySelector('.create-button');
+
+btnCreate.addEventListener('click', addButton)
