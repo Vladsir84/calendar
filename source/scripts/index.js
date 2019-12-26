596edb5cfe5254ac1b0c05d79673062ed7b84bd6
“Il_Roman” <il.roman@ukr.net>
2019-12-26 21:24:43 +0200

    red Line

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,3 +4,4 @@ import { generateNumbersRange, getTimesBlocks, } from './main.js';
 import { emptyCellClick } from './slots-logic.js';
 import { addButton, timeElem, setTimer, renderTimeList } from './popup.js';
 import { arrOfEvents } from './storage.js';
+import { redLine } from './red-line.js';
