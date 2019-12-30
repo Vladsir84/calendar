3eef8e56d1c1915852e5f883c07e3a9af45c3384
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-30 08:51:04 +0200

    Update deleteAll.js

--- source/scripts/deleteAll.js
+++ source/scripts/deleteAll.js
@@ -1,7 +1,1 @@
-const deleteAll = () => {
-    for (let i = 0; i < arrOfEvents.length - 1; i++) {
-        let a = document.querySelector('.event')
-        if (a == null) return
-        a.parentNode.removeChild(a)
-    };
-}
+
