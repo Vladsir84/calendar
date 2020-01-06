dc2f7afc20c183c5a5e9f2303e184b2bc110e436
“Il_Roman” <il.roman@ukr.net>
2020-01-06 19:17:04 +0200

    Edit Event (just display current date in popup)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,8 @@
 import { calendarRendering } from './calendar-visualization.js';
 import { createDates, renderDates, showCurrentMonthAndYear, renderNextWeek, renderPreviousWeek, today_button_function } from './navigation.js';
 import { generateNumbersRange, getTimesBlocks, } from './main.js';
-// import { emptyCellClick } from './slots-logic.js';
 import { addButton } from './popup.js';
 import { arrOfEvents } from './storage.js';
 import { redLine } from './red-line.js';
 import { saveEvent } from './save-event.js';
+import { activeEventOnclick } from './edit-event.js';
