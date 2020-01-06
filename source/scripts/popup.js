fd404de21dd09b513744cf567fa0a2699277c8a5
“Il_Roman” <il.roman@ukr.net>
2020-01-06 23:34:37 +0200

    commit

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,10 +1,20 @@
-const popupStartTime = document.querySelector('.start-time');
-const popupEndTime = document.querySelector('.end-time');
-
 export const addButton = (event) => {
     if (event.target.classList.value === 'emptyCell') {
-        popupStartTime.value = event.target.closest('.emptyRow').dataset.time + '';
-        popupEndTime.value = event.target.closest('.emptyRow').dataset.time + '';
+        let startDate = document.querySelector('.start-date');
+        let startTime = document.querySelector('.start-time');
+        let endTime = document.querySelector('.end-time');
+        let endDate = document.querySelector('.end-date');
+
+        // вставляем время в попап
+        startTime.value = event.target.closest('.emptyRow').dataset.time + '';
+        endTime.value = event.target.closest('.emptyRow').dataset.time + '';
+
+        // вставляем дату в попап
+        let startMonth = '0'
+        let startYear = '2020'
+        let startDay = event.target.dataset.day
+        startDate.value = `${startYear}-${startMonth}-${startDay}`;
+        endDate.value = `${startYear}-${startMonth}-${startDay}`;
     }
     const popup = document.querySelector(`.popup`);
     popup.classList.add('popup-switch');
