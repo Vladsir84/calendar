13bf375c8161d3b3947f99f6dfd3803c9f2e884b
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-08 15:06:26 +0200

    Update popup.js

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,15 +1,36 @@
 import { createTemporaryCell } from './temporary-cell.js';
 import { calendarRendering } from './calendar-visualization.js';
 
+let startDate = document.querySelector('.start-date');
+let startTime = document.querySelector('.start-time');
+let endTime = document.querySelector('.end-time');
+let endDate = document.querySelector('.end-date');
+
+function dateToString(date) {
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = month < 10 ? '0' + month : month;
+    let day = date.getDate();
+    day = day < 10 ? '0' + day : day;
+
+    return `${year}-${month}-${day}`;
+};
+
+function timeToString(date) {
+    let hour = date.getHours();
+    hour = hour < 10 ? '0' + hour : hour;
+    let minute = date.getMinutes();
+    minute = minute < 10 ? '0' + minute : minute;
+
+    return `${hour}:${minute}`;
+}
+
 export const addButton = (event) => {
     if (event.target.classList.value === 'emptyCell') {
-
         createTemporaryCell(event.target);
 
-        let startDate = document.querySelector('.start-date');
-        let startTime = document.querySelector('.start-time');
-        let endTime = document.querySelector('.end-time');
-        let endDate = document.querySelector('.end-date');
+
+
 
         // вставляем время в попап
         startTime.value = event.target.closest('.emptyRow').dataset.time + '';
@@ -22,9 +43,18 @@ export const addButton = (event) => {
         startDate.value = `${startYear}-${startMonth}-${startDay}`;
         endDate.value = `${startYear}-${startMonth}-${startDay}`;
     }
+
     const popup = document.querySelector(`.popup`);
     popup.classList.add('popup-switch');
+
+    startDate.value = dateToString(new Date());
+    endDate.value = dateToString(new Date());
+    startTime.value = timeToString(new Date());
+    endTime.value = timeToString(new Date());
+
 };
+
+
 
 const popupWindow = document.querySelector(`.calendar-visualization`);
 const btnCreate = document.querySelector('.create-button');
@@ -33,7 +63,7 @@ popupWindow.addEventListener('click', addButton);
 btnCreate.addEventListener('click', addButton);
 
 let closeButton = document.querySelector('.close');
-closeButton.onclick = function () {
+closeButton.onclick = function() {
     event.preventDefault();
     const popup = document.querySelector(`.popup`);
     popup.classList.remove('popup-switch');
