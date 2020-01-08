5c889841416459fd07405c1f7a9ad3d84c37f72d
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-08 11:53:55 +0200

    Update popup.js

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,122 +1,67 @@
-import { arrOfEvents } from './storage.js';
-
-const inputName = document.querySelector(`.input__name`);
-const startDates = document.querySelector(`.start-date`);
-const endDates = document.querySelector(`.end-date`);
-const startTime = document.querySelector('.start-time')
-const endTime = document.querySelector('.end-time')
-const description = document.querySelector('.description-input')
-const btnUpdate = document.querySelector('.submit-button');
-
-export const counter = (elem) => {
-    let num = elem
-    num < 10 ? num = `0${num}` : num;
-    return num
-}
+import { createTemporaryCell } from './temporary-cell.js';
+import { calendarRendering } from './calendar-visualization.js';
 
 export const addButton = (event) => {
+    if (event.target.classList.value === 'emptyCell') {
+
+        createTemporaryCell(event.target);
+
+        let startDate = document.querySelector('.start-date');
+        let startTime = document.querySelector('.start-time');
+        let endTime = document.querySelector('.end-time');
+        let endDate = document.querySelector('.end-date');
+
+        // вставляем время в попап
+        startTime.value = event.target.closest('.emptyRow').dataset.time + '';
+        endTime.value = event.target.closest('.emptyRow').dataset.time + '';
+
+        // вставляем дату в попап
+        let startMonth = '0'
+        let startYear = '2020'
+        let startDay = event.target.dataset.day
+        startDate.value = `${startYear}-${startMonth}-${startDay}`;
+        endDate.value = `${startYear}-${startMonth}-${startDay}`;
+    }
+
+    // startDate.value = dateToString(startDate);
+    // endDate.value = dateToString(endDate);
+    // startTime.value = timeToString(startDate);
+    // endTime.value = timeToString(endDate);
+
     const popup = document.querySelector(`.popup`);
     popup.classList.add('popup-switch');
-    if (event.target.classList.value === '.emptyCell') {
-        btnUpdate.classList.remove("update")
+};
 
-        const getYear = event.target.dataset.yearNumber
-        const getMonth = event.target.dataset.monthNumber
-        const getDay = event.target.dataset.dateNumber
-        const getTime = event.target.closest('.emptyRow').dataset.timeSet
 
-        inputName.value = '';
-        startDates.value = getYear + '-' + counter(getMonth) + "-" + counter(getDay);
-        endDates.value = startDates.value
-        startTime.value = `${counter(getTime-1)+':00'}`
-        endTime.value = `${counter(getTime)+':00'}`
-        description.value = ''
+export function dateToString(date) {
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = month < 10 ? '0' + month : month;
+    let day = date.getDate();
+    day = day < 10 ? '0' + day : day;
 
-        return
-    }
+    return `${year}-${month}-${day}`;
+};
 
-    if (event.target.classList.contains("add-event")) {
+export function timeToString(date) {
+    let hour = date.getHours();
+    hour = hour < 10 ? '0' + hour : hour;
+    let minute = date.getMinutes();
+    minute = minute < 10 ? '0' + minute : minute;
 
-        let dateNow = new Date();
-        inputName.value = '';
-        startDates.value = dateNow.getFullYear() + '-' + counter(dateNow.getMonth()) + "-" + counter(dateNow.getDate());
-        endDates.value = dateNow.getFullYear() + '-' + counter(dateNow.getMonth()) + "-" + counter(dateNow.getDate())
-        startTime.value = `${counter(dateNow.getHours())+':00'}`
-        endTime.value = `${counter(dateNow.getHours()+1)+':00'}`
-        description.value = ''
-
-    } else {
-        btnUpdate.classList.add("update")
-
-//         const getId = event.target.dataset.idNumber;
-//         let eventObj = arrOfEvents.find(elem => elem.id == getId);
-//         let startEventTime = new Date(arrOfEvents.startDate);
-//         let endEventTime = new Date(arrOfEvents.endDate);
-//         let year = startEventTime.getFullYear();
-//         let months = counter(startEventTime.getMonth() + 1);
-//         let day = counter(startEventTime.getDate());
-//         let valueStartHour = counter(startEventTime.getHours());
-//         let valueStartMin = counter(startEventTime.getMinutes());
-//         let valueEndHour = counter(endEventTime.getHours());
-//         let valueEndMin = counter(endEventTime.getMinutes());
-
-//         inputName.value = eventObj.name;
-//         startDates.value = year + '-' + months + '-' + day;
-//         endDates.value = startDates.value
-//         startTime.value = `${valueStartHour+':'+valueStartMin}`
-//         endTime.value = `${valueEndHour+':'+valueEndMin}`
-//         description.value = eventObj.description;
-
-//         return selectedElem = eventObj, selectedId = getId;
-    }
+    return `${hour}:${minute}`;
 }
 
-const timeListElemStart = document.querySelector('.start-time');
-const timeListElemEnd = document.querySelector('.end-time');
 const popupWindow = document.querySelector(`.calendar-visualization`);
-
 const btnCreate = document.querySelector('.create-button');
 
 popupWindow.addEventListener('click', addButton);
 btnCreate.addEventListener('click', addButton);
 
-export const setTimer = (from, to) => {
-    const result = [];
-
-    for (let i = from; i <= to; i++) {
-        result.push(i);
-    };
-    return result;
+let closeButton = document.querySelector('.close');
+closeButton.onclick = function() {
+    event.preventDefault();
+    const popup = document.querySelector(`.popup`);
+    popup.classList.remove('popup-switch');
+    calendarRendering();
 }
-
-export const timeElem = () => {
-    let resultTime = [];
-
-    setTimer(0, 23)
-        .map(timeList => {
-            let setTime = '';
-            let setMin = 0;
-            let cell = '00';
-            for (let i = 0; i < 4; i++) {
-                timeList < 10 ? setTime = `0${timeList}` : setTime = timeList;
-                resultTime.push(
-                    `<option 
-            value="${setTime}:${cell}"
-            data-block-number='${timeList}'>${setTime}:${cell}</option> `)
-                setMin += 15;
-                cell = setMin;
-            };
-        })
-
-    return resultTime.join('');
-}
-
-let selectedId;
-let selectedElem;
-
-export const renderTimeList = () => {
-    timeListElemStart.innerHTML = timeElem();
-    timeListElemEnd.innerHTML = timeElem();
-}
-
-renderTimeList()
