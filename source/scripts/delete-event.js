0461232c1d0a2ec10153f5e5e69a0b1f2ef4c66a
“Il_Roman” <il.roman@ukr.net>
2020-01-12 20:04:49 +0200

    local storage

--- source/scripts/delete-event.js
+++ source/scripts/delete-event.js
@@ -9,6 +9,7 @@ export function deleteButtonOnclick(obj) {
         for (let i = 0; i < arrOfEvents.length; i++) {
             if (obj.id == arrOfEvents[i].id) {
                 arrOfEvents.splice(i, 1);
+                savetoLocalStorage();
                 const popup = document.querySelector(`.popup`);
                 popup.classList.remove('popup-switch');
                 renderDates();
@@ -16,5 +17,4 @@ export function deleteButtonOnclick(obj) {
             }
         }
     }
-    savetoLocalStorage();
 }
