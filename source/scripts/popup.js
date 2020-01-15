04d5f5dfe49c973dee1dc80be03198cbcc424ad9
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-15 12:25:14 +0200

    Update popup.js

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -5,6 +5,7 @@ let startDate = document.querySelector('.start-date');
 let startTime = document.querySelector('.start-time');
 let endTime = document.querySelector('.end-time');
 let endDate = document.querySelector('.end-date');
+let color = document.querySelector('.event__color-picker');
 
 function dateToString(date) {
     const year = date.getFullYear();
@@ -32,6 +33,9 @@ export const addButton = (event) => {
         const popup = document.querySelector(`.popup`);
         popup.classList.add('popup-switch');
 
+        const defaultBackgroundColor = document.querySelector('.event__color-picker');
+        defaultBackgroundColor.value = '#4183f1';
+
         // вставляем время в попап
         startTime.value = event.target.closest('.emptyRow').dataset.time + '';
         endTime.value = event.target.closest('.emptyRow').dataset.time + '';
@@ -58,18 +62,22 @@ export const addButton = (event) => {
         endTime.value = timeToString(new Date());
 
     }
+
+    // const defaultBackgroundColor = document.querySelector('.event__color-picker');
+    // defaultBackgroundColor.value = '#4183f1';
+
 };
-
-
 
 const popupWindow = document.querySelector(`.calendar-visualization`);
 const btnCreate = document.querySelector('.create-button');
+// const defaultBackgroundColor = document.querySelector('.event__color-picker');
+// defaultBackgroundColor.value = '#4183f1';
 
 popupWindow.addEventListener('click', addButton);
 btnCreate.addEventListener('click', addButton);
 
 let closeButton = document.querySelector('.close');
-closeButton.onclick = function () {
+closeButton.onclick = function() {
     event.preventDefault();
     const popup = document.querySelector(`.popup`);
     popup.classList.remove('popup-switch');
